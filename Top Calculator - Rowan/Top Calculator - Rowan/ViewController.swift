69ff945111bd8454c88c40aac468627727f96dd8
rgwallin23 <rgwallin23@blakeschool.org>
2022-11-09 09:41:50 -0600

    finished slide 1 :)

---
+++ Top Calculator - Rowan/Top Calculator - Rowan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Top Calculator - Rowan
+//
+//  Created by Rowan Wallin23 on 9/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ blah/blah/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  blah
+//
+//  Created by Rowan Wallin23 on 10/24/22.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+print("\"Welcome Home!\" ")
+print("Welcome Home!")
+print("\'Welcome Home!\' ")
